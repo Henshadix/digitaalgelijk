18d293a3e2c683ccc5aac21df461d48a8ef72ef8
Your Name <your.email@example.com>
2025-03-07 13:52:17 +0100

    Fix: Server now listens on all IP addresses and uses port 3000

---
+++ server.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// Wijzig de poort van 3001 naar 3000 om overeen te komen met de Dockerfile
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  // Standaard route naar onder-constructie.html
+  let filePath = './onder-constructie.html';
+  
+  // Als de URL /onder-constructie is, gebruik ook onder-constructie.html
+  if (req.url === '/onder-constructie') {
+    filePath = './onder-constructie.html';
+  }
+
+  // Bepaal het content type op basis van de bestandsextensie
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+  
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+  }
+
+  // Lees het bestand
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // Pagina niet gevonden
+        fs.readFile('./404.html', (err, content) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf8');
+        });
+      } else {
+        // Server error
+        res.writeHead(500);
+        res.end(Server Error: );
+      }
+    } else {
+      // Succes
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
+});
+
+// Luister op alle IP-adressen (0.0.0.0) in plaats van alleen localhost
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(Server draait op http://0.0.0.0:);
+  console.log(Bezoek http://localhost:/onder-constructie om de pagina lokaal te bekijken);
+  console.log(Bezoek http://[jouw-ip-adres]:/onder-constructie om de pagina extern te bekijken);
+});
